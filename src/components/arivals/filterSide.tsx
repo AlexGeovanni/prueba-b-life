9b8163edbd3485518d5127af445a7699e3c32a72
Alex G <A180065@unach.mx>
2024-08-24 01:46:01 -0700

    Actualizando estilos que habia fallado

--- src/components/arivals/filterSide.tsx
+++ src/components/arivals/filterSide.tsx
@@ -12,37 +12,37 @@ export default function FilterSide() {
       id: 1,
       title: "Computer & Laptop",
       provider: true,
-      colorStock:"#1F2537",
-      text:"#FFFFFF",
+      colorStock:"bg-[#1F2537]",
+      text:"text-[#FFFFFF]",
       stock:15
     },
     {
       id: 2,
       title: "Mobile & Phone",
       provider: true,
-      colorStock:"#AFE638",
-      text:"#1F2537",
+      colorStock:"bg-[#AFE638]",
+      text:"text-[#1F2537]",
       stock:35
     },{
       id: 3,
       title: "Camera Imaging",
       provider: true,
-      colorStock:"#5E1EE5",
-      text:"#FFFFFF",
+      colorStock:"bg-[#5E1EE5]",
+      text:"text-[#FFFFFF]",
       stock:10
     },{
       id: 4,
       title: "TV & Smart Box",
       provider: true,
-      colorStock:"#FFFFFF",
-     text:"#1F2537",
+      colorStock:"bg-[#FFFFFF]",
+     text:"text-[#1F2537]",
       stock:8
     },{
       id: 5,
       title: "Home & Appliances",
       provider: true,
-      colorStock:"#FFE921",
-     text:"#1F2537",
+      colorStock:"bg-[#FFE921]",
+     text:"text-[#1F2537]",
       stock:2
     }
   ];
@@ -59,7 +59,7 @@ export default function FilterSide() {
                 onClick={() => handleClick(item.id)}
               >
                 {item.title}
-                <span className={`ms-5 bg-[${item.colorStock}] text-[${item.text}] p-1 px-3 rounded-md text-[15px] font-semibold`}>
+                <span className={`ms-5 ${item.colorStock} ${item.text} p-1 px-3 rounded-md text-[15px] font-semibold`}>
                   {item.stock}
                 </span>
               </li>
